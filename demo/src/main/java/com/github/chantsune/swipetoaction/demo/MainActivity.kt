52469a6638edbf62132de158c1977fc5ea904d3f
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-02-08 21:55:53 +0900

    Update MainActivity.kt

--- demo/src/main/java/com/github/chantsune/swipetoaction/demo/MainActivity.kt
+++ demo/src/main/java/com/github/chantsune/swipetoaction/demo/MainActivity.kt
@@ -6,24 +6,29 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.github.chantsune.swipetoaction.demo.databinding.ActivityMainBinding
 import com.github.chantsune.swipetoaction.views.SwipeLayout
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    var recyclerView: RecyclerView? = null
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recyclerView = findViewById<View>(R.id.recyclerView) as RecyclerView
-        recyclerView!!.layoutManager = LinearLayoutManager(this)
-        recyclerView!!.adapter = RecyclerAdapter()
-        recyclerView!!.addItemDecoration(
-            DividerItemDecoration(
-                this,
-                DividerItemDecoration.VERTICAL
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.also { recyclerView ->
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = RecyclerAdapter()
+            recyclerView.addItemDecoration(
+                DividerItemDecoration(
+                    this,
+                    DividerItemDecoration.VERTICAL
+                )
             )
-        )
+        }
 //        val swipeLayout: SwipeLayout = findViewById<View>(R.id.swipe_layout) as SwipeLayout
 //        swipeLayout.setOnSwipeItemClickListener(object : SwipeLayout.OnSwipeItemClickListener {
 //            override fun onSwipeItemClick(left: Boolean, index: Int) {
@@ -42,12 +47,17 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 //        })
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     private fun showToast(toast: String) {
         Toast.makeText(this, toast, Toast.LENGTH_SHORT).show()
     }
 
     fun collapse(view: View?) {
-        (recyclerView!!.findViewHolderForAdapterPosition(0)!!.itemView as SwipeLayout).setItemState(
+        (binding.recyclerView.findViewHolderForAdapterPosition(0)!!.itemView as SwipeLayout).setItemState(
             SwipeLayout.ITEM_STATE_COLLAPSED,
             true
         )
