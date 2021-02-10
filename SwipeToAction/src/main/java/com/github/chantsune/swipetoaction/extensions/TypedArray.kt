f249fa71febb255edbc7750743d98da8ed38d1b7
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-02-10 23:49:28 +0900

    Delete TypedArray.kt

--- SwipeToAction/src/main/java/com/github/chantsune/swipetoaction/extensions/TypedArray.kt
+++
@@ -1,9 +0,0 @@
-package com.github.chantsune.swipetoaction.extensions
-
-import android.content.res.TypedArray
-
-operator fun TypedArray.iterator() = object : Iterator<Int> {
-    private var index = 0
-    override fun hasNext() = index < indexCount
-    override fun next() = getIndex(index++)
-}
