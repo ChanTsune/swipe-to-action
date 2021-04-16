7b8a5c5f873a73247cd2a6508ab55772a9fe7549
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-04-16 22:46:48 +0900

    Update MainFragment.kt

--- demo/src/main/java/com/github/chantsune/swipetoaction/demo/main/MainFragment.kt
+++ demo/src/main/java/com/github/chantsune/swipetoaction/demo/main/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import com.github.chantsune.swipetoaction.demo.databinding.FragmentMainBinding
 
@@ -23,7 +24,7 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get()
 
         binding.goToSimpleButton.setOnClickListener {
             findNavController().navigate(MainFragmentDirections.actionMainFragmentToSimpleSwipeLayoutFragment())
