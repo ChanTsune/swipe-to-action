aff577ddabc75c525ffe98d677085fdb8e1f5dd4
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-04-18 22:55:11 +0900

    Add mail app like ui sample

--- demo/src/main/java/com/github/chantsune/swipetoaction/demo/mail/MailLayoutViewModel.kt
+++ demo/src/main/java/com/github/chantsune/swipetoaction/demo/mail/MailLayoutViewModel.kt
@@ -1,7 +1,16 @@
 package com.github.chantsune.swipetoaction.demo.mail
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.github.chantsune.swipetoaction.demo.mail.model.Mail
 
-class MailLayoutViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
+class MailLayoutViewModel(
+    var mails: LiveData<List<Mail>> = liveData {
+        emit(listOf(
+            Mail("Apple Store", "Title", "Body ".repeat(10), "18:58", false),
+            Mail("Apple Store", "Title", "Body ".repeat(20), "17:32", false),
+            Mail("Apple Store", "Title", "Body ".repeat(30), "15:43", true),
+        ))
+    }
+) : ViewModel()
